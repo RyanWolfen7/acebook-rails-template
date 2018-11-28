9ab304e357fc93455434e33e4ac5e676bb983b8e
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-28 12:24:32 +0000

    passed feature test

--- spec/features/customise_wall_spec.rb
+++ spec/features/customise_wall_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Customise wall", type: :feature do
     click_link "Themes"
     page.select("Blue", from: "colours")
     click_link "Save"
-    expect(page).to have_css('blue')
+    # find('body').value
+    expect(page).to have_css('body', visible: 'blue')
   end
-end 
+end
