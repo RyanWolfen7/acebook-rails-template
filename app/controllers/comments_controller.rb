57ac59d7e5162c2733f74a4c6fbb0fcb6b891b4f
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-29 16:17:26 +0000

    next rspec test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,15 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
+
+  def create
+    @comment = Comment.create(post_params)
+    redirect_to post_url
+  end
+
+  private
+
+  def post_params
+    params.require(:comment).permit(:message, :user_id)
+  end
 end
