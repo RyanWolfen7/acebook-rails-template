266d3e6e80cbd23e5de55cd176e24bd83aac6caf
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-30 12:46:18 +0000

    got post_id to create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
   def new
     @comment = Comment.new
+    p '#NEW DEF'
     p params
     p 'the format is:'
     p request.format.to_str
@@ -9,12 +10,16 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.create(post_params)
+    p '#CREATE DEF'
+    p params
+    p 'the format is:'
+    p request.format.to_str
     redirect_to post_url(params[:id])
   end
 
   private
 
   def post_params
-    params.require(:comment).permit(:post_id, :message, :user_id)
+    params.require(:comment).permit(:post_id ,:message, :user_id)
   end
 end
